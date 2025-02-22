a99896c5a47f60d7c27c877fdcbbb03d475728b2
UsagiiTsukino <willgotofactory@gmail.com>
2025-02-22 23:09:32 +0700

    initial nestjs project , adding controller, module, service of Posts

---
+++ src/posts/posts.service.ts
@@ -0,0 +1,20 @@
+import { Injectable } from '@nestjs/common';
+
+@Injectable()
+export class PostsService {
+  getPosts() {
+    return 'All posts';
+  }
+  getPost(id: string) {
+    return `Post ${id}`;
+  }
+  createPost(body: any) {
+    return body;
+  }
+  updatePost(id: string, body: any) {
+    return `Post ${id} updated, body ${body}`;
+  }
+  deletePost(id: string) {
+    return `Post ${id} deleted`;
+  }
+}
