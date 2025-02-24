f2a8f1cae89199323059369efcc17b62f70886ea
UsagiiTsukino <willgotofactory@gmail.com>
2025-02-25 05:58:51 +0700

    Intergrate Prisma Services on Shared Modules

--- src/routes/posts/posts.service.ts
+++ src/routes/posts/posts.service.ts
@@ -1,15 +1,25 @@
+/* eslint-disable @typescript-eslint/no-unsafe-member-access */
+/* eslint-disable @typescript-eslint/no-unsafe-assignment */
 import { Injectable } from '@nestjs/common'
+import { PrismaService } from 'src/shared/services/prisma.service'
 
 @Injectable()
 export class PostsService {
+  constructor(private readonly prismaService: PrismaService) {}
   getPosts() {
-    return 'All posts'
+    return this.prismaService.post.findMany()
   }
   getPost(id: string) {
     return `Post ${id}`
   }
   createPost(body: any) {
-    return body
+    return this.prismaService.post.create({
+      data: {
+        title: body.title,
+        content: body.content,
+        authorId: 1,
+      },
+    })
   }
   updatePost(id: string, body: any) {
     return `Post ${id} updated, body ${body}`
