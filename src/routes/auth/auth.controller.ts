19feb8821265821325a50bb10ad0c864e7d510a7
UsagiiTsukino <willgotofactory@gmail.com>
2025-03-11 03:28:35 +0700

    Intergrate NestJS interceptor

--- src/routes/auth/auth.controller.ts
+++ src/routes/auth/auth.controller.ts
@@ -1,14 +1,17 @@
 import { Body, ClassSerializerInterceptor, Controller, Post, SerializeOptions, UseInterceptors } from '@nestjs/common'
 import { AuthService } from './auth.service'
 import { RegisterBodyDTO, RegisterResDTO } from './auth.dto'
+import { TransformInterceptor } from 'src/shared/interceptor/transform.interceptor'
 
 @Controller('auth')
 export class AuthController {
   constructor(private readonly authService: AuthService) {}
-
-  @SerializeOptions({ type: RegisterBodyDTO })
+  @UseInterceptors(TransformInterceptor)
+  // @SerializeOptions({ type: RegisterBodyDTO })
   @Post('register')
-  register(@Body() body: RegisterBodyDTO) {
-    return this.authService.register(body)
+  async register(@Body() body: RegisterBodyDTO) {
+    console.log('onRegister...')
+
+    return new RegisterResDTO(await this.authService.register(body))
   }
 }
